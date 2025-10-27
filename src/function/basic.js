37f8b2aaa07f315f5256ade1939afbe556fde774
Neru <novavaux@gmail.com>
2025-10-27 22:37:04 +0000

    new

--- src/function/basic.js
+++ src/function/basic.js
@@ -1,3 +1,29 @@
+// scroll to top button 
+
+let topButton = document.getElementById("toTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+function scrollTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+}
+
+topButton.addEventListener('click', scrollTop)
+
+
+
+
+
 // to calculate the vh minus the header with responsive design in mind
 const mainHeader = document.getElementById('mainHeader');
 
